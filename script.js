c99e63392c330dd9402b7a11f3199796607299ec
Huy Nguyen <huynguyen560@gmail.com>
2024-09-22 18:14:00 -0700

    feat: add logic for displaying and hiding the modal
    Authorized-by: Huy Nguyen (huynguyen560@gmail.com)

--- script.js
+++ script.js
@@ -17,16 +17,21 @@ const myLibrary = [];
 const addBookButton = document.querySelector(".add-book-btn");
 const modal = document.querySelector(".modal");
 const submitButton = document.querySelector(".submit");
+const cancelButton = document.querySelector(".cancel");
 
 function addBookToLibrary() {}
 
 function display() {}
 
 addBookButton.addEventListener("click", () => {
-  modal.classList.remove("hidden");
+  modal.style.display = "flex";
 });
 
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
-  modal.classList.add("hidden");
+  modal.style.display = "none";
+});
+
+cancelButton.addEventListener("click", () => {
+  modal.style.display = "none";
 });
