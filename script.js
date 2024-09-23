3b100f5219c0697eca0fa24191db2eae099058f3
Huy Nguyen <huynguyen560@gmail.com>
2024-09-22 18:28:22 -0700

    feat: add logic to add new book to library
    Authorized-by: Huy Nguyen (huynguyen560@gmail.com)

--- script.js
+++ script.js
@@ -7,21 +7,35 @@ function Book(title, author, pages, read) {
 
 Book.prototype.info = function () {
   if (this.read) {
-    return `${title} by ${author}, ${this.pages} pages, has read.`;
+    return `${this.title} by ${this.author}, ${this.pages} pages, has read.`;
   }
 
-  return `${title} by ${author}, ${this.pages} pages, not yet read.`;
+  return `${this.title} by ${this.author}, ${this.pages} pages, not yet read.`;
 };
 
-const myLibrary = [];
+let myLibrary = [];
+
 const addBookButton = document.querySelector(".add-book-btn");
 const modal = document.querySelector(".modal");
 const submitButton = document.querySelector(".submit");
 const cancelButton = document.querySelector(".cancel");
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const hasReadCheckbox = document.querySelector("#read");
 
-function addBookToLibrary() {}
+function addBookToLibrary(book) {
+  myLibrary = [...myLibrary, book];
+}
 
-function display() {}
+function display(library) {}
+
+function resetInputs() {
+  titleInput.value = "";
+  authorInput.value = "";
+  pagesInput.value = "";
+  hasReadCheckbox.checked = false;
+}
 
 addBookButton.addEventListener("click", () => {
   modal.style.display = "flex";
@@ -30,8 +44,19 @@ addBookButton.addEventListener("click", () => {
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
   modal.style.display = "none";
+  const hasRead = hasReadCheckbox.checked ? true : false;
+  const newBook = new Book(
+    titleInput.value,
+    authorInput.value,
+    pagesInput.value,
+    hasRead
+  );
+  addBookToLibrary(newBook);
+
+  resetInputs();
 });
 
 cancelButton.addEventListener("click", () => {
   modal.style.display = "none";
+  resetInputs();
 });
